6ba6a420d76b8269b6326b9bb1010a3ae5d43f8b
vistamarketing <consultas.agenciavista@gmail.com>
2026-01-21 12:57:10 +0100

    v13

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -4,8 +4,8 @@ var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || p
 var config_default = defineConfig({
   branch,
   // Get this from tina.io
-  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // token: process.env.TINA_TOKEN,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
   build: {
     outputFolder: "admin",
     publicFolder: "public"
